2d95a4f0d6d8199a9545276067c2dae7b8d74122
Pranjay91 <pranjaylohbare91@gmail.com>
2022-11-05 12:13:28 +0530

    .

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { useState } from 'react';
 function App() {
   const [data,setData]=useState({height:"",weight:"",age:"",gender:""})
   function calculate(){
-   if(data.height==="" || data.height<0|| data.weight==="" || data.weight<0){
-alert("positive numbers only")
+    if(data.height==="" || data.height<0|| data.weight==="" || data.weight<0){
+    alert("positive numbers only")
    }
-   else if(data.age>120||data.age<2){
-alert("Please provide an age between 2 and 120")
+    else if(data.age>120||data.age<2){
+    alert("Please provide an age between 2 and 120")
    }
    else{
        const h =data.height/100;
-   const result=data.weight/(h*h);
-   alert(result)}
+       const result=data.weight/(h*h);
+       alert(result)}
   }
     
    
@@ -25,10 +25,10 @@ alert("Please provide an age between 2 and 120")
        <label>Age</label>
        <input onChange={(e)=>{setData({...data,age:e.target.value})}} value={data.age} name="age"/><span>2-120</span><br/></div>
        <label>Gender:</label>
-<input type="radio"   name="" value="Male"/>
-<label for="Male">Male</label>
-<input type="radio"   name="fav_language" value="Female"/>
-<label for="Female">Female</label><br/>
+        <input type="radio"   name="" value="Male"/>
+        <label for="Male">Male</label>
+        <input type="radio"   name="fav_language" value="Female"/>
+        <label for="Female">Female</label><br/>
        <label>Height</label>
        <input onChange={(e)=>{setData({...data,height:e.target.value})}} value={data.height} name="height"/><br/>
        <label >Weight</label>
